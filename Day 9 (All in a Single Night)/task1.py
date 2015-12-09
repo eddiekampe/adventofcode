79e6ba14b686aeeb166923a67d8fa54d63c54677
Eddie Kämpe <eddiek@kth.se>
2015-12-09 18:31:27 +0100

    Day 9 - Task 1

---
+++ Day 9 (All in a Single Night)/task1.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+distances = [line.rstrip("\n") for line in open("input.txt", "r")]
+travel_costs = dict()
+cities = set()
+
+for distance in distances:
+
+    city_from, _, city_to, _, cost = distance.split(" ")
+    travel_costs[(city_from, city_to)] = int(cost)
+    travel_costs[(city_to, city_from)] = int(cost)
+    cities.update([city_from, city_to])
+
+shortest = 99999999
+
+for permutation in permutations(cities):
+
+    p = list(permutation)
+    costs = sum(travel_costs[(p[i - 1], p[i])] for i, _ in enumerate(permutation) if i > 0)
+
+    if costs < shortest:
+        shortest = costs
+
+print shortest
