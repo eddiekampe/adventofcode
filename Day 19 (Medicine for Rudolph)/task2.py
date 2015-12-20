5b5d123f50bdf5f104c8ed4438d754a4987c039d
Eddie Kämpe <eddiek@kth.se>
2015-12-20 01:04:48 +0100

    Day 19 - Task 2
    Finally got solution after switching from dict to OrderedDict

---
+++ Day 19 (Medicine for Rudolph)/task2.py
@@ -0,0 +1,28 @@
+import re
+from collections import OrderedDict
+
+molecule = open("molecule.txt", "r").readline()
+replacements = [line.rstrip("\n") for line in open("input.txt", "r")]
+translations = OrderedDict()
+
+# Read input
+for replacement in replacements:
+    before, after = replacement.split(" => ")
+    translations[after] = before
+
+
+def explore(current_molecule, depth=0):
+
+    depth += 1
+    if current_molecule == "e":
+        yield depth
+
+    for t in translations.iterkeys():
+        for match in re.finditer(t, current_molecule):
+
+            new_molecule = current_molecule[0: match.start()] + translations[t] + current_molecule[match.end():]
+            for m in explore(new_molecule, depth):
+                yield m
+
+
+print next(explore(molecule))
